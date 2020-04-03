90d2889a742563211504e6cdba3d800c23de02f0
ManikandanRAP <manikandan@rap.ventures>
2020-04-03 19:07:59 +0530

    home, menu, form recognition, color and other changes

---
+++ packages/landing-gatsby/public/component---src-pages-about-js-e19aca9c92c7b6be161c.js
@@ -0,0 +1,52 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [6],
+  {
+    '3XHS': function(e, t, a) {
+      'use strict';
+      a.r(t);
+      var n = a('ERkP'),
+        r = a.n(n),
+        l = a('Wbzz'),
+        c = a('Bl7J'),
+        o = a('vrFN');
+      t.default = function() {
+        return r.a.createElement(
+          c.a,
+          null,
+          r.a.createElement(o.a, { title: 'Page two' }),
+          r.a.createElement('h1', null, 'Hi from the second page'),
+          r.a.createElement('p', null, 'Welcome to page 2'),
+          r.a.createElement('p', null, 'Some changes'),
+          r.a.createElement(l.Link, { to: '/' }, 'Go back to the homepage')
+        );
+      };
+    },
+    Bl7J: function(e, t, a) {
+      'use strict';
+      var n = a('PlB2'),
+        r = a('ERkP'),
+        l = a.n(r),
+        c = a('Wbzz');
+      t.a = function(e) {
+        var t = e.children;
+        return l.a.createElement(c.StaticQuery, {
+          query: '755544856',
+          render: function(e) {
+            return l.a.createElement(
+              l.a.Fragment,
+              null,
+              l.a.createElement('main', null, t)
+            );
+          },
+          data: n,
+        });
+      };
+    },
+    PlB2: function(e) {
+      e.exports = JSON.parse(
+        '{"data":{"site":{"siteMetadata":{"title":"Rapid Acceleration Partners AI"}}}}'
+      );
+    },
+  },
+]);
+//# sourceMappingURL=component---src-pages-about-js-e19aca9c92c7b6be161c.js.map
